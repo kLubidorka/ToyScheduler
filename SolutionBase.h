ad4bb5244ad77f6c42dd4379b8e0aca47d6b4edf
Ivan <39248996+kLubidorka@users.noreply.github.com>
2021-05-06 02:53:27 +0300

    minor fix

--- SolutionBase.h
+++ SolutionBase.h
@@ -5,7 +5,7 @@
 
 class SolutionBase {
 public:
-    virtual void solve(const std::string &input_filename, const std::string &output_filename);
+    virtual void solve(const std::string &input_filename, const std::string &output_filename) {};
 };
 
 #endif //TOYSCHEDULER_SOLUTIONBASE_H
