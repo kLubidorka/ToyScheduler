7220e8380f9615fbfec518600c6f17fb9e12d82e
Ivan <39248996+kLubidorka@users.noreply.github.com>
2021-05-06 04:02:51 +0300

    minor fix

--- Task.h
+++ Task.h
@@ -2,13 +2,13 @@
 #define TOYSCHEDULER_TASK_H
 
 struct Task {
-    unsigned appear_time;
-    unsigned priority;
-    unsigned execution_time;
-    unsigned required_resources;
+    size_t appear_time;
+    size_t priority;
+    size_t execution_time;
+    size_t required_resources;
 
-    Task(unsigned appear_time, unsigned priority,
-         unsigned execution_time, unsigned required_resources) :
+    Task(size_t appear_time, size_t priority,
+         size_t execution_time, size_t required_resources) :
             appear_time(appear_time),
             priority(priority),
             execution_time(execution_time),
--- main.cpp
+++ main.cpp
@@ -3,20 +3,24 @@
 #include "TestGenerator.h"
 #include "SolutionBase.h"
 
-void generate_tests(int test_num){
+void generate_tests(int test_num) {
     TestGenerator generator(10, 10, 100);
-    for (int i = 0; i < test_num; i++){
-        generator.generateAndPrintTest(10, "tests/test" + std::to_string(i) +  ".txt");
+    for (int i = 0; i < test_num; i++) {
+        generator.generateAndPrintTest(10, "tests/test" + std::to_string(i) + ".txt");
     }
 }
 
-void run_solution(SolutionBase solution, int test_num){
-    for (int i = 0; i < test_num; i++){
-        solution.solve("tests/test" + std::to_string(i) +  ".txt", "output/out" + std::to_string(i) +  ".txt");
+void run_solution(SolutionBase *solution, int test_num) {
+    for (int i = 0; i < test_num; i++) {
+        solution->solve("tests/test" + std::to_string(i) + ".txt", "output/out" + std::to_string(i) + ".txt");
     }
 }
+
 
 int main() {
-    generate_tests(10);
-    //run_solution();
+    int num_test = 10;
+
+    generate_tests(num_test);
+//    SomeSolution solution;
+//    run_solution(&solution, num_test);
 }
